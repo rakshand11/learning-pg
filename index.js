16135a100c007b9e655e4b44631f35a0c53301c7
rakshand <rakshand.chhikara.49@gmail.com>
2026-05-21 13:30:29 +0530

    module 2: CREATE TABLE, data types, constraints

--- index.js
+++ index.js
@@ -22,6 +22,46 @@ app.get("/db-check", async (req, res) => {
   }
 });
 
+app.get("/create-table", async (req, res) => {
+  try {
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS users (
+          id          SERIAL PRIMARY KEY,
+          name        VARCHAR(100) NOT NULL,
+          email       VARCHAR(100) UNIQUE NOT NULL,
+          age         INTEGER,
+          is_active   BOOLEAN DEFAULT true,
+          created_at  TIMESTAMP DEFAULT NOW()
+        )
+      `);
+    res.json({ message: "users table created!" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.get("/check-table", async (req, res) => {
+  try {
+    const result = await pool.query(`
+        SELECT column_name, data_type 
+        FROM information_schema.columns
+        WHERE table_name = 'users'
+      `);
+    res.json({ columns: result.rows });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.get("/drop-table", async (req, res) => {
+  try {
+    await pool.query("DROP TABLE IF EXISTS users");
+    res.json({ message: "users table dropped!" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
